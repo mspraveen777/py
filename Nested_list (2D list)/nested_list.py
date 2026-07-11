52dd5716ea83d7de9228c57795a9510224cdabd7
mspraveen777 <mspraveen7777@gmail.com>
2026-07-11 20:50:29 +0530

    Nested list -1

---
+++ Nested_list (2D list)/nested_list.py
@@ -0,0 +1,17 @@
+# 3X3
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+# print(matrix[0])
+# print(matrix[1])
+# print(matrix[1][2])
+
+for i in range(0,3):
+    for j in range(0,3):
+        print(matrix[i][j],end=" ")
+    print()
+
