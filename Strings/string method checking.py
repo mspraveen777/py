e328b7c2c431a2c175d0f13b1cb24171ecb23ef1
Praveen Muttappa Sunagar <mspraveen7777@gmail.com>
2026-07-22 12:49:07 +0530

    string methods

--- Strings/string method checking.py
+++ Strings/string method checking.py
@@ -1,1 +1,22 @@
 # for checking
+
+text = "Praveen Sunagar"
+
+# print(text.isalpha())
+# print(text.isdigit())
+# print(text.isalnum())
+# print(text.isspace())
+# print(text.startswith("P"))
+print(text.endswith("gar"))
+
+
+#example
+
+age = input("Enter the age: ")
+if age.isdigit():
+    if int(age) >= 18:
+        print("You can vote")
+    else:
+        print("You cannot vote")
+else:
+    print("Plz enter proper age")
---
+++ Strings/string methods searching & finding.py
@@ -0,0 +1,7 @@
+# searching and finding
+
+text  = "Praveen Sunagar is a coder"
+# print(text.count(" "))
+# print(text.find("z"))
+# print(text.index("z"))
+print(text.replace("z","v"))
